4db7e2c701e1ba2722e875c43d1325b58bc4c6e5
Andrew Davis <atd285@gmail.com>
2019-03-06 18:36:03 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RenderList from './components/RenderList';
 
 class App extends Component {
+  state = {
+    data: [],
+    country: '',
+    capital: '',
+    region: '',
+    subregion: ''
+  };
+  componentDidMount() {
+    this.getLocations();
+  }
+  getLocations = () => {
+    const API_URL = 'https://raw.githubusercontent.com/mledoze/countries/master/countries.json';
+    return fetch(API_URL)
+      .then((res) => res.json())
+      .then((result) => {
+        let data = result.map((element, indx, arr) => {
+          return {
+            country: element.name.official,
+            capital: element.capital[0],
+            region: element.region,
+            subregion: element.subregion,
+            lat: element.latlng[0],
+            lng: element.latlng[1]
+          };
+        });
+        this.setState(() => ({
+          data
+        }));
+      });
+  };
+  handleCountryChange = (e) => {
+    const val = e.target.value;
+    this.setState(() => ({
+      country: val
+    }));
+  };
+  handleCapitalChange = (e) => {
+    const val = e.target.value;
+    this.setState(() => ({
+      capital: val
+    }));
+  };
+  handleRegionChange = (e) => {
+    const val = e.target.value;
+    this.setState(() => ({
+      region: val
+    }));
+  };
+  handleSubregionChange = (e) => {
+    const val = e.target.value;
+    this.setState(() => ({
+      subregion: val
+    }));
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <div className='header'>
+          <h1>Table Search</h1>
+        </div>
+        <div className='search-container'>
+          <input type='text' name='country' placeholder='Filter by Country' onChange={this.handleCountryChange} />
+          <input type='text' name='capital' placeholder='Filter by Capital' onChange={this.handleCapitalChange} />
+          <input type='text' name='region' placeholder='Filter by Region' onChange={this.handleRegionChange} />
+          <input type='text' name='subregion' placeholder='Filter by Subregion' onChange={this.handleSubregionChange} />
+        </div>
+        <RenderList
+          data={this.state.data}
+          country={this.state.country}
+          capital={this.state.capital}
+          region={this.state.region}
+          subregion={this.state.subregion}
+        />
       </div>
     );
   }
